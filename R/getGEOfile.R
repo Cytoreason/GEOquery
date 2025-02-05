84d170462400f7a34d16c1508b510227511eb541
MatanAvraham <matan.avraham@cytoreason.com>
2025-02-05 14:38:20 +0000

    restore gunzip function

--- R/getGEOfile.R
+++ R/getGEOfile.R
@@ -168,3 +168,59 @@ downloadFile <- function(url, destfile, mode, quiet = TRUE) {
   }
   return(0)
 }
+
+#' Gunzip a file
+#'
+#' gunzip a file
+#'
+#' This function was stripped out of R.utils due to breaking some stuff on the
+#' bioconductor build machine.
+#'
+#' @param filename The filename to be unzipped
+#' @param destname The destination file
+#' @param overwrite Boolean indicating whether or not to overwrite a destfile
+#' of the same name
+#' @param remove Boolean indicating whether or not to remove the original file
+#' after completion
+#' @param BFR.SIZE The size of the read buffer....
+#' @return Invisibly, the number of bytes read.
+#' @author Original author: Henrik Bengtsson
+#' @seealso \code{\link{gzfile}}
+#' @keywords IO
+#'
+#' @examples
+#'
+#' # gunzip('file.gz',remove=FALSE)
+#'
+#'
+#' @export
+gunzip <- function(filename, destname=gsub("[.]gz$", "", filename), overwrite=FALSE, remove=TRUE, BFR.SIZE=1e7) {
+  if (filename == destname)
+    stop(sprintf("Argument 'filename' and 'destname' are identical: %s", filename));
+  if (!overwrite && file.exists(destname))
+    stop(sprintf("File already exists: %s", destname));
+
+  inn <- gzfile(filename, "rb");
+  on.exit(if (!is.null(inn)) close(inn));
+
+  out <- file(destname, "wb");
+  on.exit(close(out), add=TRUE);
+
+  nbytes <- 0;
+  repeat {
+    bfr <- readBin(inn, what=raw(0), size=1, n=BFR.SIZE);
+    n <- length(bfr);
+    if (n == 0)
+      break;
+    nbytes <- nbytes + n;
+    writeBin(bfr, con=out, size=1);
+  };
+
+  if (remove) {
+    close(inn);
+    inn <- NULL;
+    file.remove(filename);
+  }
+
+  invisible(nbytes);
+}
